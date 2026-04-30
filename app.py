6d455c60d06b8c2d4c96ddeaa47565a592beadab
joshuaswart000 <joshuaswart000@gmail.com>
2026-04-29 17:28:23 -0700

    Add '-reg' flag to subprocess.Popen command

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class DoomGame:
         # Added stderr capture to see why it might be hanging
         try:
             self.process = subprocess.Popen(
-                [DOOM_PATH, "-wad", WAD_PATH, "-nocolor", "-i"],
+                [DOOM_PATH, "-wad", WAD_PATH, "-nocolor", "-i", "-reg"],
                 stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
